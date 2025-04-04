a96c215f916f009571195d26e08aacc28582b224
Druhan Shah <druhanrshah@gmail.com>
2025-04-04 15:23:55 +0530

    Emergency Backup

--- model/model.py
+++ model/model.py
@@ -1,331 +1,111 @@
-"""
-Full definition of a GPT Language Model, all of it in this single file.
-References:
-1) the official GPT-2 TensorFlow implementation released by OpenAI:
-https://github.com/openai/gpt-2/blob/master/src/model.py
-2) huggingface/transformers PyTorch implementation:
-https://github.com/huggingface/transformers/blob/main/src/transformers/models/gpt2/modeling_gpt2.py
-"""
 import math
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-
-
-def set_seed(seed=0):
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-
-
-def top_k_top_p_filtering(probs, top_k=0, top_p=0.0, filter_value=-float('Inf')):
-    """ Filter a distribution of logits using top-k and/or nucleus (top-p) filtering
-        Args:
-            logits: logits distribution shape (vocabulary size)
-            top_k >0: keep only top k tokens with highest probability.
-            top_p >0.0: keep the top tokens with cumulative probability >= top_p.
-        Basic outline taken from
-        https://gist.github.com/thomwolf/1a5a29f6962089e871b94cbd09daf317
-    """
-    assert probs.dim() == 2  # [BATCH_SIZE, VOCAB_SIZE]
-    top_k = min(top_k, probs.size(-1))
-    if top_k > 0:
-        indices_to_remove = probs < torch.topk(probs, top_k, dim=1)[0][..., -1, None]
-        probs[indices_to_remove] = filter_value
-    
-    sorted_probs, sorted_indices = torch.sort(probs, descending=True)
-    
-    cumulative_probs = torch.cumsum(sorted_probs, dim=-1)
-    sorted_indices_to_remove = cumulative_probs > top_p
-    sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[..., :-1].clone()
-    sorted_indices_to_remove[..., 0] = 0
-    
-    sorted_probs[sorted_indices_to_remove] = filter_value
-    probs = torch.gather(sorted_probs, 1, sorted_indices.argsort(-1))
-    
-    pred_token = torch.multinomial(probs, 1) # [BATCH_SIZE, 1]
-    return pred_token
+from transformer_lens import HookPoint()
+from einops import repeat
 
 
 class LayerNorm(nn.Module):
-    """
-    LayerNorm but with an optional bias.
-    PyTorch doesn't support simply bias=False
-    """
-    def __init__(self, ndim, bias):
+
+    def __init__(self, cfg):
+       super().__init__()
+       self.cfg = cfg
+       self.eps = self.cfg.eps
+       self.len = self.cfg.d_model
+
+       self.w = nn.Parameter(torch.ones(self.len, dtype=self.cfg.dtype))
+       self.b = nn.Parameter(torch.zeros(self.len, dtype=self.cfg.dtype))
+
+       self.hook_scale = HookPoint() # [b, pos, 1]
+       self.hook_norm = HookPoint() # [b, pos, len]
+
+    def forward(self, x):
+        x -= x.mean(-1, keepdim=True)
+        scale = self.hook_scale((x.pow(2).mean(-1, keepdim=True) + self.eps).sqrt())
+        x /= scale
+        return self.hook_norm(x * self.w + self.b).to(self.cfg.dtype)
+
+
+class Attention(nn.Module):
+
+    def __init__(self, cfg):
         super().__init__()
-        self.weight = nn.Parameter(torch.ones(ndim))
-        self.bias = nn.Parameter(torch.zeros(ndim)) if bias else None
-
-    def forward(self, input):
-        return F.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-5)
+        self.cfg = cfg
+        self.mask = torch.tril(torch.ones((self.cfg.n_ctx, self.cfg.n_ctx)).bool())
 
 
-class CausalSelfAttention(nn.Module):
-    """
-    One operation of multi-head self attention (MHSA).
-    Calculate Query, Key, Value and pass through MHSA
-    """
+    def attention(self, q_in, k_in, v_in, mask, drop=None):
+        q = q_in * self.W_q + self.b_q
+        k = k_in * self.W_k + self.b_k
+        v = v_in * self.W_v + self.b_v
 
-    def __init__(self, config):
-        super().__init__()
-        assert config.n_embd % config.n_head == 0
+        return F.scaled_dot_product_attention(q, k, v, mask, dropout_p=drop)
 
-        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
-        self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
-
-        self.n_head = config.n_head
-        self.n_embd = config.n_embd
-
-    def forward(self, x, get_attn_map=False):
-        """
-        Compute self attention output to be added to residual stream
-        """
-        B, T, C = x.size()
-
-        q, k, v  = self.c_attn(x).split(self.n_embd, dim=2)
-        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
-        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
-        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
-
-        # efficient attention using Flash Attention CUDA kernels
-        if get_attn_map:
-            attn_map = self.get_attention(query=q, key=k)
-            torch.cuda.empty_cache()
-
-        y = torch.nn.functional.scaled_dot_product_attention(
-            q, k, v, attn_mask=None,
-            dropout_p=0,
-            is_causal=True)
-
-        y = self.c_proj(y.transpose(1, 2).contiguous().view(B, T, C))
-
-        if get_attn_map:
-            return y, attn_map
-        else:
-            return y
-
-    def get_attention(self, query, key) -> torch.Tensor:
-        L, S = query.size(-2), key.size(-2)
-        scale_factor = 1 / math.sqrt(query.size(-1))
-        attn_bias = torch.zeros(L, S, dtype=query.dtype, device=query.device)
-        temp_mask = torch.ones(L, S, dtype=torch.bool, device=query.device).tril(diagonal=0)
-        attn_bias.masked_fill_(temp_mask.logical_not(), float("-inf"))
-        attn_bias.to(query.dtype)
-
-        attn_weight = query @ key.transpose(-2, -1) * scale_factor
-        attn_weight += attn_bias
-        attn_weight = torch.softmax(attn_weight, dim=-1)
-        return attn_weight
+    def forward(self, x):
+        attention = 
 
 
 class MLP(nn.Module):
-    def __init__(self, config):
+
+    def __init__(self, cfg):
         super().__init__()
-        self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
-        self.gelu    = nn.GELU()
-        self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd, bias=config.bias)
+        self.cfg = cfg
+
+
+class HookedEncoder(nn.Module):
+
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+
+        self.attn = Attention(self.cfg)
+        self.ln1 = LayerNorm(self.cfg)
+        self.mlp = MLP(self.cfg)
+        self.ln2 = LayerNorm(self.cfg)
+
+        self.hook_q_input = HookPoint()  # [b, pos, n_h, d_m]
+        self.hook_k_input = HookPoint()  # [b, pos, n_h, d_m]
+        self.hook_v_input = HookPoint()  # [b, pos, n_h, d_m]
+
+        self.hook_attn_out = HookPoint()  # [b, pos, d_m]
+        self.hook_mlp_out = HookPoint()  # [b, pos, d_m]
+        self.hook_resid_pre = HookPoint()  # [b, pos, d_m]
+        self.hook_resid_mid = HookPoint()  # [b, pos, d_m]
+        self.hook_resid_mid_norm = HookPoint()  # [b, pos, d_m]
+        self.hook_resid_post = HookPoint()  # [b, pos, d_m]
+        self.hook_resid_post_norm = HookPoint()  # [b, pos, d_m]
 
     def forward(self, x):
-        x = self.c_fc(x)
-        x = self.gelu(x)
-        x = self.c_proj(x)
-        return x
+        resid_pre = self.hook_resid_pre(x)
 
+        q = self.hook_q_input(repeat(resid_pre, "bpd -> bpnd", n=self.cfg.n_h))
+        k = self.hook_k_input(repeat(resid_pre, "bpd -> bpnd", n=self.cfg.n_h))
+        v = self.hook_v_input(repeat(resid_pre, "bpd -> bpnd", n=self.cfg.n_h))
 
-class Block(nn.Module):
-    """
-    One self-attention block
-    """
-    def __init__(self, config):
-        super().__init__()
-        self.ln_1 = LayerNorm(config.n_embd, bias=config.bias)
-        self.attn = CausalSelfAttention(config)
-        self.ln_2 = LayerNorm(config.n_embd, bias=config.bias)
-        self.config = config
-        if config.mlp:
-            self.mlp = MLP(config)
-
-    def forward(self, x):
-        """
-        Add to residual stream after self-attention and MLP.
-        """
-        x = x + self.attn(self.ln_1(x))
-        if self.config.mlp:
-            x = x + self.mlp(self.ln_2(x))
-        return x
-
-    def fwd_to_attn_map(self, x):
-        """
-        Add to residual stream after self-attention and MLP.
-        """
-        y, attn_map = self.attn(self.ln_1(x), get_attn_map=True)
-        x = x + y
-        if self.config.mlp:
-            x = x + self.mlp(self.ln_2(x))
-        return x, attn_map
-
-
-class GPT(nn.Module):
-    def __init__(self, config, vocab_size):
-        super().__init__()
-        self.config = config
-
-        self.transformer = nn.ModuleDict(dict(
-            wte = nn.Embedding(vocab_size, config.n_embd),
-            wpe = nn.Embedding(config.context_size, config.n_embd),
-            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
-            ln_f = LayerNorm(config.n_embd, bias=config.bias),
+        attention = self.hook_attn_out(self.attn(
+            q, k, v,
+            attention_mask=attention_mask
         ))
-        self.LM_head = nn.Linear(config.n_embd, vocab_size, bias=False)
-        self.transformer.wte.weight = self.LM_head.weight # Weight tying
+        resid_mid = self.hook_resid_mid(resid_pre + attention)
+        resid_mid_norm = self.hook_resid_mid_norm(self.ln1(resid_mid))
 
-        self.apply(self._init_weights) # init
-        for pn, p in self.named_parameters(): # apply scaled init to residuals
-            if pn.endswith('c_proj.weight'):
-                torch.nn.init.normal_(
-                    p, mean=0.0, std=0.02/math.sqrt(2 * config.n_layer))
+        mlp_out = self.hook_mlp_out(self.mlp(resid_mid_norm))
+        resid_post = self.hook_resid_post(resid_mid_norm + mlp_out)
+        resid_post_norm = self.hook_resid_post_norm(self.ln2(resid_post))
 
-    def get_num_params(self, non_embedding=True):
-        """
-        Return the number of parameters in the model.
-        For non-embedding count (default), the position embeddings get subtracted.
-        The token embeddings would too, except due to the parameter sharing these
-        params are actually used as weights in the final layer, so we include them.
-        """
-        n_params = sum(p.numel() for p in self.parameters())
-        if non_embedding:
-            n_params -= self.transformer.wpe.weight.numel()
-        return n_params
-
-    def _init_weights(self, module):
-        if isinstance(module, nn.Linear):
-            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
-            if module.bias is not None:
-                torch.nn.init.zeros_(module.bias)
-        elif isinstance(module, nn.Embedding):
-            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
-
-    def forward(self, inputs):
-        device = inputs.device
-        b, t = inputs.size()
-
-        # Compute position/token embeddings
-        tok_emb = self.transformer.wte(inputs)
-        pos = torch.arange(0, t, dtype=torch.long, device=device)
-        pos_emb = self.transformer.wpe(pos)
-
-        x = tok_emb + pos_emb
-        for block in self.transformer.h:
-            x = block(x)
-        x = self.transformer.ln_f(x)
-        logits = self.LM_head(x)
-        return logits 
+        return resid_post_norm
 
 
-    @torch.no_grad()
-    def next_token_probs(self, inputs, logprobs=False, prune_vocab=None):
-        """
-        Compute the probability of the next token given a batch of inputs
-        """
-        logits = self.forward(inputs)
-        logits = logits[:, -1, :] # [B, L, V] -> [B, 1, V]
-        if prune_vocab is not None:
-            logits = logits[:, :prune_vocab]
-        if logprobs:
-            return F.log_softmax(logits, -1)
-        else:
-            return F.softmax(logits, -1)
+class HookedModel(nn.Module):
 
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
 
-    @torch.no_grad()
-    def per_token_dist(self, inputs, logprobs=False, prune_vocab=None):
-        """
-        Compute the next-token distribution given a batch of inputs
-        """
-        logits = self.forward(inputs) # [B, L, V]
-        if prune_vocab is not None:
-            logits = logits[:, :, :prune_vocab]
-        if logprobs:
-            return F.log_softmax(logits, -1)
-        else:
-            return F.softmax(logits, -1)
+        self.blocks = nn.ModuleList([HookedEncoder(self.cfg)
+                                     for _ in range(self.cfg.n_layers)])
+        self.class_head = nn.Linear(self.cfg.d_m, 2)
 
-
-    @torch.no_grad()
-    def get_loglikelihoods(self, sequences, pad_token_id, reduction_type='sum'):
-        """
-        Compute likelihood of a batch of input samples and corresponding labels
-        """
-        inputs, labels = sequences[:, :-1].clone(), sequences[:, 1:].clone()
-        labels[labels == pad_token_id] = -100  # Mask padding
-
-        logits = self.forward(inputs)
-
-        ll = - F.cross_entropy(
-            logits.reshape(-1, logits.size(-1)),
-            labels.reshape(-1),
-            ignore_index=-100, 
-            reduction=reduction_type,
-            )
-        return ll
-    
-    
-    @torch.no_grad()
-    def sample(self, inputs, max_new_tokens, n_states=None, 
-               sampling_strategy='stochastic', retrieve_llhoods=None):
-        """
-        Sample from the model given a batch of inputs
-        """
-        if retrieve_llhoods is not None:
-            per_token_llhoods = []
-
-        for t in range(max_new_tokens):
-            probs = self.next_token_probs(inputs, logprobs=False, prune_vocab=n_states)
-
-            # Sample next token
-            if sampling_strategy == 'stochastic':
-                next_token = torch.multinomial(probs, 1)
-            elif sampling_strategy == 'greedy':
-                next_token = torch.argmax(probs, -1, keepdims=True)
-            elif sampling_strategy == 'top_k_top_p':
-                next_token = top_k_top_p_filtering(probs, top_k=5, top_p=0.9)
-            else:
-                raise ValueError("Invalid sampling strategy")
-            
-            inputs = torch.cat((inputs, next_token), dim=1)
-
-            if retrieve_llhoods is not None:
-                llhood = torch.log(probs.gather(1, next_token)).squeeze()
-                per_token_llhoods.append(llhood)
-
-        if retrieve_llhoods == 'tokens':
-            per_token_llhoods = torch.stack(per_token_llhoods, dim=1)
-            return inputs, per_token_llhoods
-        elif retrieve_llhoods == 'sequences':
-            llhoods = per_token_llhoods.sum(dim=1)
-            return inputs, llhoods
-        else:
-            return inputs
-    
-
-    @torch.no_grad()
-    def get_attention_map(self, inputs):
-        """
-        Compute attention map for a batch of input samples
-        """
-        attn_maps = {}
-        device = inputs.device
-        b, t = inputs.size()
-
-        tok_emb = self.transformer.wte(inputs)
-        pos = torch.arange(0, t, dtype=torch.long, device=device)
-        pos_emb = self.transformer.wpe(pos)
-
-        x = tok_emb + pos_emb
-        for b_id, block in enumerate(self.transformer.h):
-            x, attn_map = block.fwd_to_attn_map(x)
-            attn_maps[b_id] = attn_map
-
-        return attn_maps
+    def forward(self, x):
+        pass
