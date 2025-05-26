b83447b9748df733e9b532e68da71e4bb0c91c5b
Druhan Shah <druhanrshah@gmail.com>
2025-05-26 12:59:26 +0530

    fix: import error in utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
 from .logging import init_wandb, set_seed, open_log, cleanup
 from .logging import save_model, sanity_checks, log_gen, log_train, log_eval
 from .optimizer import configure_optimizers, update_cosine_warmup_lr, move_to_device
-from .obj import DictToObj, obj_to_dict
+from .obj import dict_to_obj, obj_to_dict
